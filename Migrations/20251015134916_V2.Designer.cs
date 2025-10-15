214b7f4b42f585cab256a10204beb95275075191
LuisFelipeUnzueta <lfunzueta@gmail.com>
2025-10-15 15:03:18 -0300

    adjust

--- Migrations/20251015134916_V2.Designer.cs
+++ Migrations/20251015134916_V2.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Minimal.Api.Infra.Db;
 
@@ -10,9 +11,11 @@ using Minimal.Api.Infra.Db;
 namespace Minimal.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251015134916_V2")]
+    partial class V2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251015134916_V2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Minimal.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class V2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Rule",
+                table: "Admins",
+                newName: "Role");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Role",
+                table: "Admins",
+                newName: "Rule");
+        }
+    }
+}
